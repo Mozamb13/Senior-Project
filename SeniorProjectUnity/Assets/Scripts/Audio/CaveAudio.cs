6855f4bf64dacd53b71f54d1ba6f111921237657
Anna E <anna.edmondson17@gmail.com>
2020-03-02 13:33:51 -0700

    Auto stash before merge of "AnnaDevelop" and "DarthObsidian/master"

---
+++ SeniorProjectUnity/Assets/Scripts/Audio/CaveAudio.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.Audio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveAudio : MonoBehaviour
+{
+    public AudioSource audio;
+
+    void OnTriggerEnter(Collider other){
+        if(other.tag == "Player"){
+            audio.Play();
+        }
+    }
+}
